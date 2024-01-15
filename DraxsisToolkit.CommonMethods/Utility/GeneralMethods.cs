e63d125f4034deb84e7fd31b41a9a6612e032f2e
Mostafa Koolabadi <draxsis.1995@gmail.com>
2024-01-15 20:00:09 +0330

    Add files via upload

---
+++ DraxsisToolkit.CommonMethods/Utility/GeneralMethods.cs
@@ -0,0 +1,9 @@
+﻿namespace DraxsisToolKit.Utility;
+
+public static class GeneralMethods
+{
+    public static void Sleep(int milisec = 100)
+    {
+        System.Threading.Thread.Sleep(milisec);
+    }
+}
